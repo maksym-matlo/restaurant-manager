894fff8a1f6a87ea9d6377d66e89db4935cd4076
Maksym Matlo <maksym.matlo@gmail.com>
2022-06-11 11:47:25 +0300

    Added common exception

---
+++ restaurant-manager-impl/src/main/java/com/maximalus/exception/RestaurantManagerException.java
@@ -0,0 +1,13 @@
+package com.maximalus.exception;
+
+/**
+ * RestaurantManagerException.
+ *
+ * @author Maksym Matlo
+ */
+public class RestaurantManagerException extends RuntimeException {
+    public RestaurantManagerException(String message) {
+        super(message);
+    }
+}
+
